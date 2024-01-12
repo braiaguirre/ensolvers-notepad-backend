ee6484a0b00242ccbdc922c45feb9485442eb967
braiaguirre <brianaguirre.sp94@gmail.com>
2024-01-12 16:38:56 -0300

    ssl fix

--- src/db.js
+++ src/db.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
+const { pg } = require('pg');
 const { POSTGRES_URL_NON_POOLING } = process.env;
 
 const noteModel = require('./models/noteModel');
 const tagModel = require('./models/tagModel');
 
 const sequelize = new Sequelize(
-    `${POSTGRES_URL_NON_POOLING}?sslmode=require`,
-    { logging: false, native: false });
+    `${POSTGRES_URL_NON_POOLING}?sslmode=require`,  
+    { logging: false, native: false, dialect: "postgres", dialectModule: pg });
 
 noteModel(sequelize);
 tagModel(sequelize);
