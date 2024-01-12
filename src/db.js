e96b927b0a39304ac2c7c9ae8b1e4f844f6a568b
braiaguirre <brianaguirre.sp94@gmail.com>
2024-01-12 16:18:32 -0300

    upload

--- src/db.js
+++ src/db.js
@@ -8,7 +8,7 @@ const tagModel = require('./models/tagModel');
 const sequelize = new Sequelize(
     `postgres://${ DB_USER }:${ DB_PASSWORD }@${ DB_HOST }/${ DB_NAME }`,
     { logging: false, native: false });
-
+//TEST
 noteModel(sequelize);
 tagModel(sequelize);
 
