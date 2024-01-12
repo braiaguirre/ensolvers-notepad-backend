5751352e049a3dcb5785889a050bb32e0b235331
braiaguirre <brianaguirre.sp94@gmail.com>
2024-01-12 16:38:11 -0300

    ssl fix

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { POSTGRES_URL_NON_POOLING } = process.env;
 
 const noteModel = require('./models/noteModel');
 const tagModel = require('./models/tagModel');
