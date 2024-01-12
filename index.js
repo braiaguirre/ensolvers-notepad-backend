8cc154dd7c869a1f7eecb212ea60187d9cca858a
braiaguirre <brianaguirre.sp94@gmail.com>
2024-01-12 16:43:06 -0300

    fix

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const { conn } = require("./src/db");
 
 const PORT = process.env.PORT;
 
-server.listen(PORT, () => {
-    conn.sync({ force: false }).then(()=>{ console.log(`Connected to the DB: ${process.env.DB_NAME}`) });
-    console.log(`Server raised in port: ${ PORT }`);
+// server.listen(PORT, () => {
+//     conn.sync({ force: false }).then(()=>{ console.log(`Connected to the DB: ${process.env.DB_NAME}`) });
+//     console.log(`Server raised in port: ${ PORT }`);
+//   });
+
+const startServer = async() => {
+    try {
+    await conn.sync({ force: false })
+    server.listen(PORT, () => {
+    console.log(`Server raised in port: ${PORT}`);
   });
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+startServer()
